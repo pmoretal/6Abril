aa3ceaa15d56a96277ccb14038e8270d75f7dde1
UX430 <UX430@LAPTOP-BK2DPCSO>
2022-04-13 11:49:02 +0200

    Inicio

--- src/Entornos/Ejemplo2.java
+++ src/Entornos/Ejemplo2.java
@@ -3,7 +3,8 @@ package Entornos;
 public class Ejemplo2 {
 
 	public static void main(String[] args) {
-		System.out.println("hola mundo, Pablo");
+		System.out.println("hola mundo");
+		System.out.println("********Pablo*************");
 	}
 
 }
